339c73712b1cef2caf3ed0aff150b616297ede4e
ikt1 <IAN@Alan-Tomkinsons-iMac.local>
2016-03-30 15:04:39 -0500

    dynamics, integration

--- Lab3 MATLAB/simulateRR.m
+++ Lab3 MATLAB/simulateRR.m
@@ -42,7 +42,8 @@ dt = 0.01; % [s]
 t_f = 10; % [s]
 
 % Initial Conditions
-X_0 = [0; 0];
+X_0 = [pi/3; 0; pi/2; 0];
+% [theta1 theta1' theta2 theta2']
 
 % Control Gains (Scalar)
 K_p = 1;
@@ -50,32 +51,42 @@ K_v = 1;
 
 % Numerical Integration
 t = 0:dt:t_f;
-X = []; % initialize variable to hold state vector
-X_dot = []; % initialize variable to hold state vector derivatives
+X = zeros(4,length(t)); % initialize variable to hold state vector
+X_dot = zeros(4,length(t)); % initialize variable to hold state vector derivatives
+thetad=[pi/3;0;pi/2;0];
 for i = 1:length(t)
     if i == 1
-
+        X(i,:)=X_0;
     else
-
+        %Use X calculated from previous calculation
     end
     
     % Control torques
-    tau = [];
+    tau = -K_p*(X([1,3],i-1)-thetad)-K_v*X([2,4],i-1);
     
     % Apply joint torque limits
     tau(tau>tau_max) = tau_max;
     tau(tau<-tau_max) = -tau_max;
     
     % Dynamic Model
-    M = [M1*Lc1^2+M2*(l_1+Lc2*cos(X(2)))^2+I1+I2, 0 ; 0, M2*Lc2^2+I2];
-    C = [-2*M2*Lc2*sin(X(2))*(l_1+Lc2*cos(X(2)))*X_dot(1)*X_dot(2);...
-        M2*Lc2*sin(X(2))*(l_1+Lc2*cos(X(2)))*X_dot(1)^2];
-    G = [0;M2*g*Lc2*cos(X(2))];
+    M = [M1*Lc1^2+M2*(l_1+Lc2*cos(X(3,i-1)))^2+I1+I2, 0 ; 0, M2*Lc2^2+I2];
+    C = [-2*M2*Lc2*sin(X(3,i-1))*(l_1+Lc2*cos(X(3,i-1)))*X(2,i-1)*X(4,i-1);...
+        M2*Lc2*sin(X(3,i-1))*(l_1+Lc2*cos(X(3,i-1)))*X(2,i-1)^2];
+    G = [0;M2*g*Lc2*cos(X(3,i-1))];
 
-    X_dot(i,:) = [];
+    thetapp=inv(M)*(tau-C-G)
+    X_dot(:,i) = [0;thetapp(1);0;thetapp(2)];
+    
     
     % Trapezoidal Integration
     if i > 1
+        X_dot(1,i)=X_dot(1,i-1)+.5*(X_dot(2,i-1)+X_dot(2,i))*dt;
+        X_dot(3,i)=X_dot(3,i-1)+.5*(X_dot(4,i-1)+X_dot(4,i))*dt;
+        X(2,i)=X_dot(1,i);
+        X(4,i)=X_dot(3,i);
+        
+        X(1,i)=X(1,i-1)+.5*(X(2,i-1)+X(2,i))*dt;
+        X(3,i)=X(3,i-1)+.5*(X(4,i-1)+X(4,i))*dt;
         
     end
     
